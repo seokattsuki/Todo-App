843c12f8c94133c5ee86f2789cd32ae501799f1a
Rohit Patra <info.rohitpatra@gmail.com>
2025-09-05 23:42:27 +0530

    Initial commit

--- server/src/controllers/todo.controllers.js
+++ server/src/controllers/todo.controllers.js
@@ -46,7 +46,7 @@ const createTodo = async (req, res, next) => {
         }
         const newTodo = await Todo.create(TodoData);
         return res.status(201)
-        .json({newTodo})
+        .json({message:"New Todo created",Todo:newTodo})
     } catch (error) {
         next(error)
     }
@@ -57,19 +57,62 @@ const updateTodo = async (req, res, next) => {
     try {
         const { id } = req.params;
         const updateData = req.body;
+
         if(!updateData || Object.keys(updateData).length === 0){
             return res.status(400)
             .json({message: "NO update  data provided" });
         }
+        
         const updatedTodo = await Todo.findByIdAndUpdate(id, updateData, {new: true, runValidators: true});
         if(!updatedTodo){
              return res.status(404)
             .json({message: "Todo not Found"});
         }else{
             return res.status(200)
-            .json({updatedTodo});
+            .json({message: "Todo updated successffully",Todo:updatedTodo});
         }
     } catch (error) {
         next(error)
     }
 }
+
+//delete a todo
+const deleteTodo = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const deleteTodo = await Todo.findByIdAndDelete(id);
+        if(!deleteTodo){
+            return res.status(404)
+            .json({message: "Todo not found"})
+        }
+
+        return res.status(200)
+        .json({message: "Todo deleted successfully",todo:deleteTodo})
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+// mark as completed
+const markAsCompleted = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const todo = await Todo.findById(id)
+        if(!todo){
+            return res.status(404)
+            .json({message: "todo not found"})
+        }
+
+        todo.completed = true;
+        await todo.save();
+
+        return res.status(200)
+        .json({message: "Marked as completed", todo})
+    } catch (error) {
+        next(error)
+    }
+}
+
